8524515363409942c1be2073d6e4b1dc1d76c485
Charles De Lean <cdelean@xprima.com>
2015-08-27 14:53:37 -0400

    Made compute_partner easier to maintain. Takes care of removing duplicates.

--- partner_history/partner.py
+++ partner_history/partner.py
@@ -25,38 +25,90 @@ from openerp.tools.translate import _
 from openerp.tools import ustr
 
 
-class mail_message(orm.Model):
+class mail_message(models.Model):
     _inherit = 'mail.message'
 
-    def compute_partner(self, cr, uid, active_model='res.partner', model='mail.message', res_id=1, context=None):
+    def compute_partner(
+        self, cr, uid,
+        active_model='res.partner', model='mail.message', res_id=1,
+        context=None
+    ):
+        """
+        Finds all 'active_model' references in 'model/res_id' instance
+        """
+
         if context is None:
             context = {}
-        target_ids = []
-        current_obj = self.pool.get(model)
-        cr.execute("SELECT name FROM ir_model_fields WHERE relation='" + active_model + "' and model = '" + model + "' and ttype not in ('many2many', 'one2many');")
-        for name in cr.fetchall():
-            current_data = current_obj.read(cr, uid, res_id, [str(name[0])],context=context)
-            if current_data.get(str(name[0])):
-                var = current_data.get(str(name[0]))
-                if var:
-                    target_ids.append(var[0])
 
-        cr.execute("select name, model from ir_model_fields where relation='" + model + "' and ttype in ('many2many') and model = '" + active_model + "';")
-        for field, model in cr.fetchall():
-            field_data = self.pool.get(model) and self.pool.get(model)._columns.get(field, False) \
-                            and (isinstance(self.pool.get(model)._columns[field], fields.many2many) \
-                            or isinstance(self.pool.get(model)._columns[field], fields.function) \
-                            and self.pool.get(model)._columns[field].store) \
-                            and self.pool.get(model)._columns[field] \
-                            or False
-            if field_data:
-                model_m2m, rel1, rel2 = field_data._sql_names(self.pool.get(model))
-                requete = "SELECT "+rel1+" FROM "+ model_m2m+" WHERE "+ rel2+" ="+str(res_id)+";"
-                cr.execute(requete)
-                sec_target_ids = cr.fetchall()
-                for sec_target_id in sec_target_ids:
-                    target_ids.append(sec_target_id[0])
-        return target_ids
+        target_ids = set()
+
+        # Check references directly in message table (many2one and one2one).
+
+        current_obj = self.pool.get(model)
+        cr.execute((
+            "SELECT name FROM ir_model_fields WHERE relation='{active_model}' "
+            "and model = '{model}' and ttype not in ('many2many', 'one2many');"
+        ).format(active_model=active_model, model=model))
+
+        for name in cr.fetchall():
+            name = str(name[0])
+            current_data = current_obj.read(
+                cr, uid, res_id, [name], context=context)
+
+            var = current_data.get(name)
+
+            if var:
+                target_ids.add(var[0])
+
+        # Check references in 'active_model' m2m relation
+        # tables pointing to model/res_id
+
+        current_obj = self.pool.get(active_model)
+
+        cr.execute((
+            "select name from ir_model_fields where relation='{model}' "
+            " and ttype in ('many2many') and model='{active_model}';"
+        ).format(active_model=active_model, model=model))
+
+        for field in cr.fetchall():
+
+            field = str(field[0])
+
+            if not current_obj:
+                continue
+
+            column = current_obj._columns.get(field)
+
+            if not column or not (
+                isinstance(column, fields.Many2many)
+                or isinstance(column, osv_fields.many2many)
+                or isinstance(column, osv_fields.function)
+                    and column.store
+            ):
+                continue
+
+            field_data = current_obj._columns[fcomield]
+
+            if not field_data:
+                continue
+
+            # Read from the m2m relation table
+
+            model_m2m, rel1, rel2 = field_data._sql_names(current_obj)
+            requete = (
+                "SELECT {rel1} FROM {model_m2m} WHERE {rel2}={res_id};"
+            ).format(
+                rel1=rel1,
+                model_m2m=model_m2m,
+                rel2=rel2,
+                res_id=res_id)
+
+            cr.execute(requete)
+            sec_target_ids = cr.fetchall()
+            for sec_target_id in sec_target_ids:
+                target_ids.add(sec_target_id[0])
+
+        return list(target_ids)
 
     @api.depends('model')
     def _get_object_name(self):
